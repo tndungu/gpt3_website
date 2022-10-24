8d111a2f6e42883351c09a9b6f96c7eacd8048a9
Antony Ndungu <tmndungu@gmail.com>
2022-10-24 08:05:01 +0200

    CSS Gradient

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 import {CTA, Brand, Navbar } from './components';
-import './App.css'
+import './App.css';
+import './index.css';
 
 export const App = () => {
   return (
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './navbar.css'
+import {RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
  const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className='gpt3__navbar-links_logo'>
+      	  <img src={logo} alt='Logo'/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
